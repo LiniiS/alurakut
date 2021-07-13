9f07e6e5af5e749605022e2122719143535c87a5
LiniiS <enila404@gmail.com>
2021-07-13 16:07:41 -0300

    ajustes

--- pages/index.js
+++ pages/index.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import styled from 'styled-components';
 import MainGrid from '../src/components/MainGrid';
 import Box from '../src/components/Box';
-import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
+import { AlurakutMenu, AlurakutProfileSidebarMenuDefault, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons';
 import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations';
 
 function ProfileSidebar(propriedades) {
   return (
     <Box>
       <img src={`https://github.com/${propriedades.githubUser}.png`} style={{borderRadius: '8px'}}/>
+      <hr />
+      <p>
+        <a className="boxLink" href={`https://github.com/${propriedades.githubUser}`}>
+          @{propriedades.githubUser}
+        </a>
+      </p>
+      <hr />
+      <AlurakutProfileSidebarMenuDefault />
     </Box> 
   )
 }
-
-const Title = styled.h1`
-  font-size: 50px;
-  color: ${({ theme }) => theme.colors.primary};
-`
 
 export default function Home() {
  const githubUser = 'LiniiS';
@@ -46,7 +48,7 @@ export default function Home() {
               {pessoasFavoritas.map((itemAtual) => {
                 return(
                   <li>
-                    <a href={`/useers/${itemAtual}`} key={itemAtual}>
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
                       <img src={`https://github.com/${itemAtual}.png`}/>
                       <span>{itemAtual}</span>
                     </a>
